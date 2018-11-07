5a23c8b2a35ba2d0694ed252b5b30121e14c8a7e
Pieter De Rycke <pieter_de_rycke@hotmail.com>
2018-11-07 05:52:27 +0100

    added a rest service to the project

---
+++ rest-service/runserver.py
@@ -0,0 +1,22 @@
+"""
+This script runs the rest_service application using a development server.
+"""
+
+from os import environ
+from flask import render_template
+import connexion
+
+# Create the application instance
+app = connexion.FlaskApp(__name__, specification_dir='./')
+
+# Read the swagger.yml file to configure the endpoints
+app.add_api('swagger.yml')
+app.run(port=8080)
+
+#if __name__ == '__main__':
+#    HOST = environ.get('SERVER_HOST', 'localhost')
+#    try:
+#        PORT = int(environ.get('SERVER_PORT', '5555'))
+#    except ValueError:
+#        PORT = 5555
+#    app.run(HOST, PORT)
